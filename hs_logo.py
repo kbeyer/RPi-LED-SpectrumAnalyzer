0295212358c32373f7d13471b986d74565a5deb2
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-07-29 12:54:23 -0400

    Minor clean ups to run.py.

--- hs_logo.py
+++ hs_logo.py
@@ -1,43 +1,40 @@
 from raspledstrip.color import SysColors, color_hex
-from raspledstrip.ledstrip import LEDStrip
 
-
-def draw_logo():
-    led = LEDStrip(100)
+def draw_logo(led_strip):
 
     background_color = SysColors.off
     dark_border = color_hex("#0f173c")
     dark_green = SysColors.white25 #color_hex("2e3b21")
     light_green = color_hex("#6bc325") # SysColors.green
 
-    led.fill(background_color)
+    led_strip.fill(background_color)
 
-    led.fill(dark_border, 28, 32)
-    led.fill(dark_border, 35, 37)
-    led.fill(dark_border, 39, 39)
-    led.fill(dark_border, 43, 44)
-    led.fill(dark_border, 48, 48)
-    led.fill(dark_border, 50, 53)
-    led.fill(dark_border, 55, 55)
-    led.fill(dark_border, 59, 60)
-    led.fill(dark_border, 64, 64)
-    led.fill(dark_border, 66, 68)
-    led.fill(dark_border, 71, 75)
+    led_strip.fill(dark_border, 28, 32)
+    led_strip.fill(dark_border, 35, 37)
+    led_strip.fill(dark_border, 39, 39)
+    led_strip.fill(dark_border, 43, 44)
+    led_strip.fill(dark_border, 48, 48)
+    led_strip.fill(dark_border, 50, 53)
+    led_strip.fill(dark_border, 55, 55)
+    led_strip.fill(dark_border, 59, 60)
+    led_strip.fill(dark_border, 64, 64)
+    led_strip.fill(dark_border, 66, 68)
+    led_strip.fill(dark_border, 71, 75)
 
-    led.fill(light_green, 41, 41)
-    led.fill(light_green, 57, 57)
-    led.fill(light_green, 61, 61)
+    led_strip.fill(light_green, 41, 41)
+    led_strip.fill(light_green, 57, 57)
+    led_strip.fill(light_green, 61, 61)
 
-    led.fill(dark_green, 42,42)
-    led.fill(dark_green, 45,47)
-    led.fill(dark_green, 40,40)
-    led.fill(dark_green, 62,63)
-    led.fill(dark_green, 58,58)
-    led.fill(dark_green, 56,56)
+    led_strip.fill(dark_green, 42,42)
+    led_strip.fill(dark_green, 45,47)
+    led_strip.fill(dark_green, 40,40)
+    led_strip.fill(dark_green, 62,63)
+    led_strip.fill(dark_green, 58,58)
+    led_strip.fill(dark_green, 56,56)
 
-    led.fill(SysColors.off, 0, 19)
-    led.fill(SysColors.off, 20, 27)
-    led.fill(SysColors.off, 76, 83)
-    led.fill(SysColors.off, 84)
+    led_strip.fill(SysColors.off, 0, 19)
+    led_strip.fill(SysColors.off, 20, 27)
+    led_strip.fill(SysColors.off, 76, 83)
+    led_strip.fill(SysColors.off, 84)
 
-    led.update()
+    led_strip.update()
--- run.py
+++ run.py
@@ -1,104 +1,149 @@
-# A simple run.py for the demo
+""" Main entry point for running the demo. """
 
+
+# Standard library
 import time
 import sys
+
+# Third party library
+import alsaaudio as aa
+
+# Local library
+from char import show_text
+from hs_logo import draw_logo
 from leds import ColumnedLEDStrip
 from music import calculate_levels, read_musicfile_in_chunks, calculate_column_frequency
 from shairplay import initialize_shairplay, shutdown_shairplay, RaopCallbacks
-import alsaaudio as aa
-import random
-from hs_logo import draw_logo
 
-if len(sys.argv) > 1:
-    path = sys.argv[1]
-else:
-    path = 'sample.mp3'
-
-columns = 12
-gap_leds = 0
-total_leds = 100
-skip_leds = 4
+COLUMNS = 12
+GAP_LEDS = 0
+TOTAL_LEDS = 100
+SKIP_LEDS = 4
 
 SAMPLE_RATE = 44100
 NUM_CHANNELS = 2
 FORMAT = aa.PCM_FORMAT_S16_LE
 PERIOD_SIZE = 2048
 
-led = ColumnedLEDStrip(leds=total_leds, columns=columns, gap_leds=gap_leds, skip_leds=skip_leds)
-led.all_off()
-draw_logo()
-time.sleep(1)
-
-frequency_limits =  calculate_column_frequency(200, 10000, columns)
+frequency_limits =  calculate_column_frequency(200, 10000, COLUMNS)
 
 
-input = aa.PCM(aa.PCM_CAPTURE, aa.PCM_NONBLOCK)
-input.setchannels(NUM_CHANNELS)
-input.setformat(aa.PCM_FORMAT_S16_BE)
-input.setrate(SAMPLE_RATE)
-input.setperiodsize(PERIOD_SIZE)
+def analyze_airplay_input(led_strip):
+    from os.path import join
+    lib_path = join(sys.prefix, 'lib')
+    initialize_shairplay(lib_path, get_shairplay_callback_class(led_strip))
 
-while True:
-    size, chunk = input.read()
-    if size > 0:
-        # make sure chunk is even length
-        L = (len(chunk)/2 * 2)
-        chunk = chunk[:L]
-        data = calculate_levels(chunk, SAMPLE_RATE, frequency_limits)
-        print data
-        led.display_data(data[::-1])
+    while True:
+        try:
+            pass
+        except KeyboardInterrupt:
+            shutdown_shairplay()
+            break
 
-import sys
-sys.exit(1)
+def analyze_audio_file(led_strip, path):
+    for chunk, sample_rate in read_musicfile_in_chunks(path, play_audio=True):
+        data = calculate_levels(chunk, sample_rate, frequency_limits)
+        led_strip.display_data(data)
 
-# for chunk, sample_rate in read_musicfile_in_chunks(path, play_audio=True):
-#     # data = calculate_levels(chunk, sample_rate, frequency_limits)
-#     # data = [16 * random.random()] * columns
-#     data = [1600] * columns
-#     led.display_data(data)
+def analyze_line_in(led_strip, hacker_school=True):
+    start_time = time.time()
 
-# import sys
-# sys.exit(1)
+    while True:
+        if hacker_school and time.time() - start_time > 60 * 2:
+            hacker_school_display()
+            start_time = time.time()
 
-class SampleCallbacks(RaopCallbacks):
-    def audio_init(self, bits, channels, samplerate):
-        print "Initializing", bits, channels, samplerate
-        self.bits = bits
-        self.channels = channels
-        self.samplerate = samplerate
-
-        min_frequency = 500
-        max_frequency = samplerate / 30 * 10  # Abusing integer division
-        self.frequency_limits = calculate_column_frequency(
-            min_frequency, max_frequency, columns
-        )
-        self.buffer = ''
-
-    def audio_process(self, session, buffer):
-        data = calculate_levels(buffer, self.samplerate, self.frequency_limits, self.channels, self.bits)
-        led.display_data(data[::-1])
-    def audio_destroy(self, session):
-        print "Destroying"
-    def audio_set_volume(self, session, volume):
-        print "Set volume to", volume
-    def audio_set_metadata(self, session, metadata):
-        print "Got", len(metadata),  "bytes of metadata"
-    def audio_set_coverart(self, session, coverart):
-        print "Got", len(coverart), "bytes of coverart"
+        size, chunk = input.read()
+        if size > 0:
+            L = (len(chunk)/2 * 2)
+            chunk = chunk[:L]
+            data = calculate_levels(chunk, SAMPLE_RATE, frequency_limits)
+            led_strip.display_data(data[::-1])
 
 
-path = "/home/pi/spectrum-analyzer/shairplay/src/lib/.libs/"
-initialize_shairplay(path, SampleCallbacks)
+def get_audio_input():
+    input = aa.PCM(aa.PCM_CAPTURE, aa.PCM_NONBLOCK)
+    input.setchannels(NUM_CHANNELS)
+    input.setformat(aa.PCM_FORMAT_S16_BE)
+    input.setrate(SAMPLE_RATE)
+    input.setperiodsize(PERIOD_SIZE)
+    return input
 
-while True:
-    try:
-        pass
-    except KeyboardInterrupt:
-        shutdown_shairplay()
-        break
+def get_led_strip():
+    led = ColumnedLEDStrip(
+        leds=TOTAL_LEDS, columns=COLUMNS, gap_leds=GAP_LEDS, skip_leds=SKIP_LEDS
+    )
+    led.all_off()
+    return led
+
+def get_shairplay_callback_class(led_strip):
+
+    class SampleCallbacks(RaopCallbacks):
+        def audio_init(self, bits, channels, samplerate):
+            print "Initializing", bits, channels, samplerate
+            self.bits = bits
+            self.channels = channels
+            self.samplerate = samplerate
+
+            min_frequency = 500
+            max_frequency = samplerate / 30 * 10  # Abusing integer division
+            self.frequency_limits = calculate_column_frequency(
+                min_frequency, max_frequency, COLUMNS
+            )
+            self.buffer = ''
+
+        def audio_process(self, session, buffer):
+            data = calculate_levels(buffer, self.samplerate, self.frequency_limits, self.channels, self.bits)
+            led_strip.display_data(data[::-1])
+        def audio_destroy(self, session):
+            print "Destroying"
+        def audio_set_volume(self, session, volume):
+            print "Set volume to", volume
+        def audio_set_metadata(self, session, metadata):
+            print "Got", len(metadata),  "bytes of metadata"
+        def audio_set_coverart(self, session, coverart):
+            print "Got", len(coverart), "bytes of coverart"
+
+    return SampleCallbacks
+
+def hacker_school_display(led_strip):
+    draw_logo(led_strip)
+    time.sleep(1)
+    show_text(led_strip, 'NEVER GRADUATE!', x_offset=3, y_offset=1, sleep=0.5)
 
 
-# TODO:
-# - height normalization fixes.
-# - command line to listen on stream or not
-# - minor changes to install.sh to install shairplay
+if __name__ == '__main__':
+    from textwrap import dedent
+
+    input_types = ('local', 'linein', 'airplay')
+    usage = dedent("""\
+
+    Usage: %s <input-type> [additional arguments]
+
+    input-type: should be one of %s
+
+    To play a local file, you can pass the path to the file as an additional
+    argument.
+
+    """) % (sys.argv[0], input_types)
+
+    if len(sys.argv) == 1:
+        print usage
+        sys.exit(1)
+
+    input_type = sys.argv[1]
+
+    led_strip = get_led_strip()
+    if input_type == 'local':
+        path = sys.argv[2] if len(sys.argv) > 2 else 'sample.mp3'
+        analyze_audio_file(led_strip, path)
+
+    elif input_type == 'airplay':
+        analyze_airplay_input(led_strip)
+
+    elif input_type == 'linein':
+        analyze_line_in(led_strip)
+
+    else:
+        print usage
+        sys.exit(1)
