ec6da5c9e24f7f9af848f3d7f84ad3d1e277ca18
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-07-29 12:22:55 -0400

    Cleaned up char.py.

--- char.py
+++ char.py
@@ -1,10 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
+import time
+import numpy as np
 import os
 fontpath = "/usr/share/fonts/truetype/freefont"
 
 
 from raspledstrip.ledstrip import LEDStrip
-from raspledstrip.color import wheel_color, SysColors
+from raspledstrip.color import SysColors, color_hex
 
 
 def xy_to_led_coordinates(x, y, origin_led=99, strip_along='y', x_length=12, y_length=8):
@@ -43,86 +45,22 @@ def txt2img(label, fontname="FreeMono.ttf", fgcolor=0,
 
     return imgOut
 
+def show_text(led, text, x_offset=0, y_offset=0, sleep=0.5):
+    for char in text:
+        img = txt2img(char)
+        arr = np.array(img.getdata()).reshape(img.size[::-1]).T[::, ::-1]
 
-# class CharLEDStrip(LEDStrip):
+        for x, y in np.argwhere(arr < 100):
+            led_num = xy_to_led_coordinates(x+x_offset, y-y_offset)
+            led.set(led_num, color_hex("#6bc325"))
+        led.update()
+        time.sleep(sleep)
+        led.all_off()
 
-#     def __init__(self, leds=32, columns=3, gap_leds=4, skip_leds=0):
-#         LEDStrip.__init__(self, leds, True)
-#         self.driver.spi.max_speed_hz = 7000000 #12000000
-#         print 'Changed spi freq to %d' % self.driver.spi.max_speed_hz
-#         self.columns = columns
-#         self._column_data = [0] * columns
-#         self._gap_leds = gap_leds # + 1
-#         self._skip_leds = skip_leds
-#         self._column_leds = (leds - skip_leds - (self._gap_leds * (columns - 1)))/columns
-#         print 'Leds per col: %d' % self._column_leds
-#         self._color = 0.0
-
-#     def _normalize_height(self, height, h_min=3, h_range=13):
-#         # fixme: h_min = 9, h_range = 1, in the example code.
-#         height = (height - h_min) / float(h_range)
-#         if height < 0.05:
-#             height = 0.05
-
-#         elif height > 1.0:
-#             height = 1.0
-
-#         return height
-
-#     def _get_color(self):
-#         color = wheel_color(int(self._color))
-#         self._color = self._color + 1 if self._color <= 383.9 else 0.0
-#         return color
-
-#     def display_data(self, data, color=None):
-#         """Data is a list of heights.
-
-#         The number of columns should be equal to the number of columns!  We
-#         could improve this, based on how we use it.
-
-#         """
-
-#         self.fillOff()
-
-#         if color is None:
-#             color = SysColors.red
-
-#         for i, value in enumerate(data[self._skip_leds:]):
-#             if value > 0:
-#                 self.set(i+self._skip_leds, color)
-
-#         self.update()
 
 
 if __name__ == '__main__':
-    assert xy_to_led_coordinates(0, 0) == 99
-    assert xy_to_led_coordinates(4, 6) == 61
-    assert xy_to_led_coordinates(5, 5) == 57
-    assert xy_to_led_coordinates(11, 0) == 4
-    assert xy_to_led_coordinates(10, 0) == 19
-    assert xy_to_led_coordinates(10, 7) == 12
-
-    from raspledstrip.ledstrip import LEDStrip
-    import numpy as np
-    import time
-
     led = LEDStrip(100)
     led.all_off()
 
-    for char in 'A':
-        img = txt2img(char)
-        img.save('/tmp/foo.png', format='PNG')
-        arr = np.array(img.getdata()).reshape(img.size)[::, ::-1]
-        print arr
-        print
-        print (arr < 10) * 1
-        print
-        print arr[1]
-        print
-        continue
-        for x, y in np.argwhere(arr < 100):
-            led_num = xy_to_led_coordinates(x, y)
-            print led_num
-            led.set(led_num, SysColors.red)
-        time.sleep(1)
-        led.update()
+    show_text(led, 'NEVER GRADUATE!', x_offset=3, y_offset=1)
